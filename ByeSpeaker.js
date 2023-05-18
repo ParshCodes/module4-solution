4486a92bae1f5047dba5accc45fcdeaeae8911fd
ParshCodes <20btrse025@jainuniversity.ac.in>
2023-05-19 00:06:02 +0530

    module4-soln

---
+++ ByeSpeaker.js
@@ -0,0 +1,13 @@
+(function(window){
+
+var byeSpeaker={};
+var speakword ="Good Bye";
+
+byeSpeaker.speak=function(name){
+    console.log(speakword+" "+name);
+}
+window.byeSpeaker = byeSpeaker;
+
+
+
+})(window);
---
+++ HelloSpeaker.js
@@ -0,0 +1,13 @@
+(function(window){
+
+var helloSpeaker={};
+var speakword ="Hello";
+
+helloSpeaker.speak=function(name){
+    console.log(speakword+" "+name);
+}
+window.helloSpeaker = helloSpeaker;
+
+
+
+})(window);
